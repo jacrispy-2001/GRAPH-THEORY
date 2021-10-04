fb539c3a856763dee2f615af3f1f3763ef9624a0
jacrispy-2001 <72371129+jacrispy-2001@users.noreply.github.com>
2021-10-04 10:36:00 +0530

    uncommented and added c code for IO
    uncommented and added c code for IO

--- hhh.cpp
+++ hhh.cpp
@@ -1,14 +1,15 @@
-/*
-// Sample code to perform I/O:
+
+/*Sample code to perform I/O:*/
 #include <stdio.h>
 
 int main(){
+	cout<<"input a given number:";
 	int num;
 	scanf("%d", &num);              			// Reading input from STDIN
 	printf("Input number is %d.\n", num);       // Writing output to STDOUT
 }
 
 // Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-*/
+
 
 // Write your code here
